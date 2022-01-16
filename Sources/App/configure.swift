a294c066ce4bd711007707e061027cc38f7881f2
gisrayelyan <gisrayelyan@level.co>
2022-01-16 21:06:53 +0400

    change username

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -11,7 +11,7 @@ public func configure(_ app: Application) throws {
     app.databases.use(.postgres(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "postgres",
+        username: Environment.get("DATABASE_USERNAME") ?? "vapor",
         password: Environment.get("DATABASE_PASSWORD") ?? "Asdasd90",
         database: Environment.get("DATABASE_NAME") ?? "stickers"
     ), as: .psql)
