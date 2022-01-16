d7fa251191a9ef45fefd612c050ebd00000b63c1
gisrayelyan <gisrayelyan@level.co>
2022-01-16 20:38:44 +0400

    change db host

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,7 +9,7 @@ public func configure(_ app: Application) throws {
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
     app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
+        hostname: Environment.get("DATABASE_HOST") ?? "167.99.206.87",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "postgres",
         password: Environment.get("DATABASE_PASSWORD") ?? "Asdasd90",
