ecd1a9b669128887813aac9205842bb4e517776d
gisrayelyan <gisrayelyan@level.co>
2022-01-16 21:22:31 +0400

    change db

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -13,7 +13,7 @@ public func configure(_ app: Application) throws {
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
         username: Environment.get("DATABASE_USERNAME") ?? "postgres",
         password: Environment.get("DATABASE_PASSWORD") ?? "Asdasd90",
-        database: Environment.get("DATABASE_NAME") ?? "stickers"
+        database: Environment.get("DATABASE_NAME") ?? "postgres"
     ), as: .psql)
 
     app.migrations.add(CreateSticker())
